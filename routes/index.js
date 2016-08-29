ef037171ee16b5b4669f36b229b2688efafcd4cc
Antti Luoto <antti.l.luoto@tut.fi>
2016-08-29 13:55:28 +0300

    Removed hard coded ids.

--- routes/index.js
+++ routes/index.js
@@ -23,18 +23,30 @@ var db = mongo.db( mongoURL, {native_parser:true});
 
 
 client.on('connect', function () {
-  //subscribe to new app descriptions
-  client.subscribe('device/57bee704fb2ccf266063044c/app/descr');
-  
-  //client.publish('device/debug', 'debug', {retain: true});
-  //update certain app
+
+    //Subscribe to new app descriptions.
+    //Get registered devices from the db.
+    db.collection('device').find( {} ).toArray(function(err, items){
+        if(err){
+            console.log(err.toString());
+        } else {
+            _.each( items, function ( device ) {
+                client.subscribe('device/' + device._id + '/app/descr');
+            });
+        }
+    });
+    //client.publish('device/debug', 'debug', {retain: true});
+    //update certain app
 });
 
 client.on('message', function (topic, message) {
 
     var app = JSON.parse(message);
+    var arrStr = topic.split('/');
+    console.log(arrStr);
+    var id = arrStr[1];
 
-    var query = { '_id': toObjectID( '57bee704fb2ccf266063044c' ) };
+    var query = { '_id': toObjectID( id ) };
     var update = { '$push': { 'apps': app } };
     var options = { returnOriginal: false };
     db.collection( 'device' ).findOneAndUpdate( query, update, options, function ( err, result ) {
@@ -45,8 +57,8 @@ client.on('message', function (topic, message) {
         }
         
         if ( result.lastErrorObject.n == 0 ) {
-            console.log('Device with id ' + '57bee704fb2ccf266063044c' +' not found.');
-            //return res.status( 404).send( { 'message': 'Device with id ' + '57bee4145b3d4a22dc8fe0f7' +' not found.' } );
+            console.log('Device with id ' + id +' not found.');
+            //return res.status( 404).send( { 'message': 'Device with id ' + id +' not found.' } );
         }
         
         if ( !result.lastErrorObject.updatedExisting ) {
